7d486d4a1bb89a0b2b8dc31773bad719653b4490
Ashlee Webb <ashlee.webb.18@gmail.com>
2023-06-07 14:28:06 -0400

    Update episode to have local storage

--- src/components/EpisodesGrid/Episode/Episode.js
+++ src/components/EpisodesGrid/Episode/Episode.js
@@ -1,30 +1,55 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import '../Episode/Episode.css';
 import { Link } from 'react-router-dom';
 import fin from '../../../assets/fin.svg';
 
-const Episode = ({ episodeImg, episodeId, episodeTitle }) => {
-  const [isFavorite, setIsFavorite] = useState(false);
+class Episode extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isFavorite: false
+    };
+  }
 
-  const handleFinClick = () => {
-    setIsFavorite(!isFavorite);
+  componentDidMount() {
+    const { episodeId } = this.props;
+    const isEpisodeFavorite = localStorage.getItem(episodeId);
+    if (isEpisodeFavorite) {
+      this.setState({ isFavorite: true });
+    }
+  }
+
+  handleFinClick = () => {
+    const { episodeId } = this.props;
+    const { isFavorite } = this.state;
+    this.setState({ isFavorite: !isFavorite });
+    if (!isFavorite) {
+      localStorage.setItem(episodeId, 'true');
+    } else {
+      localStorage.removeItem(episodeId);
+    }
   };
 
-  return (
-    <div className='wrapper'>
-      <div className={`polaroid${isFavorite ? ' favorite' : ''}`}>
-        <Link className="link" key={episodeId} to={`/${episodeId}`}>
-          <img className="episode-img" src={episodeImg} id={episodeId} alt={episodeTitle} />
-          <p className="episode-title">{episodeTitle}</p>
-        </Link>
-        <div className="fin-button-container" onClick={handleFinClick}>
-          {isFavorite ? <span className="fin-text">Fin</span> : null}
-          <img className="fin-button" src={fin} alt="fin" />
-          {isFavorite ? <span className="favorite-text">vorite</span> : null}
+  render() {
+    const { episodeImg, episodeId, episodeTitle } = this.props;
+    const { isFavorite } = this.state;
+
+    return (
+      <div className='wrapper'>
+        <div className={`polaroid${isFavorite ? ' favorite' : ''}`}>
+          <Link className="link" key={episodeId} to={`/${episodeId}`}>
+            <img className="episode-img" src={episodeImg} id={episodeId} alt={episodeTitle} />
+            <p className="episode-title">{episodeTitle}</p>
+          </Link>
+          <div className="fin-button-container" onClick={this.handleFinClick}>
+            {isFavorite ? <span className="fin-text">Fin</span> : null}
+            <img className="fin-button" src={fin} alt="fin" />
+            {isFavorite ? <span className="favorite-text">vorite</span> : null}
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Episode;
