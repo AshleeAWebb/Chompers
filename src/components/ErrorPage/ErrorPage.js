e3a12e65ec0e61dc886876306e94178312c61b27
Ashlee Webb <ashlee.webb.18@gmail.com>
2023-06-07 22:05:19 -0400

    Add error page

---
+++ src/components/ErrorPage/ErrorPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./ErrorPage.css";
+import errorImage from "../../assets/error-page.png";
+import { Link } from "react-router-dom";
+
+export const ErrorPage = () => {
+  return (
+    <section className="error-page">
+        <Link to="/" className="error-link">
+        Error: Just when you thought it was safe to surf the web... 
+        Your  page has vanished into the deep. 
+        Let's swim back to familiar shores.
+        </Link>
+    </section>
+  );
+};
